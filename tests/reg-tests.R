b1df41bea0d0bd8285cc0bfd023af7330aeda687
lukasz.daniel@gmail.com <lukasz.daniel@gmail.com@3ddc6fe4-3682-99f2-5bdb-9563d755b174>
2014-11-01 13:11:15 +0000

    Updated tests.

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -2,51 +2,46 @@
 str <- function(object, ...) UseMethod("str")
 
 #2: if-else
-if(length(larg <- list(...))) {
- nl <- names(larg)
- iGiveHead <- which(nl == "give.head")
- if (any(Bgl <- nl == "give.length"))
-  give.length <- larg[[which(Bgl)]]
- else if(length(iGiveHead))
-  give.length <- larg[[iGiveHead]]
- if(length(iGiveHead)) # eliminate it from arg.list
-  larg <- larg[ - iGiveHead ]
- if(is.numeric(larg[["nest.lev"]]) &&
-  is.numeric(larg[["vec.len"]])) # typical call from data.frame
- ## reduce length for typical call:
- larg[["vec.len"]] <-
-	min(larg[["vec.len"]],
-	    (larg[["width"]]- nchar(larg[["indent.str"]]) -31)%/% 19)
+a <- 1:10
+if(length(a)) {
+ b <- sum(a)
+ if (b > 100 && b < 10000)
+  c <- sqrt(b)
+ else if(b < 500)
+  c <- log(b)
 }
 
 #3: backtick
 `%w/o%` <- function(x,y) x[is.na(match(x,y))]
 
 #4: for loop
-for (i in seq_len(min(list.len,le) ) ) {
-		    cat(indent.str, comp.str, nam.ob[i], ":", sep = "")
-		    envir <- # pass envir for 'promise' components:
-			if(typeof(object[[i]]) == "promise") {
-			    structure(object, nam= as.name(nam.ob[i]))
-			} # else NULL
-		    strSub(object[[i]], give.length=give.length,
-                           nest.lev = nest.lev + 1,
-                           indent.str = paste(indent.str,".."))
+for (i in seq_len(10)) {
+		    cat(i, "\n", sep = "")
+		    e <- # dummy variable
+			if(i == 3L) {
+			    cat(sqrt(i), "\n", sep = "")
+			} # else
+		    cat(i^2, "\n", sep = "")
 }
 
 #5: while loop
+a <- 1
 while(a<10) {
  cat("Hello\n")
+ a <- a + 1
 }
 
 #6: repeat loop
 repeat {
-       error= -0.2*log((1/runif(60, 0, 1))-1) # a random component
-       z=(p<0.5+error) # TRUE/FALSE condition
-       z=replace(z, z==TRUE, 1) # replace z to 1 if z is true, else z=0
+       p <- 1
+       i <- 1
+       error= -0.2*log((1/runif(60, 0, 1))-1)
+       z=(p<0.5+error)
+       z=replace(z, z==TRUE, 1)
 
-       if (sum(z[(3*i-2):(3*i)])>0) {break}
+       if (sum(z[(3*i-2):(3*i)]) != 0) {break}
 }
 #7: command
+p <- 1
 error= -0.2*log((1/runif(60, 0, 1))-1) # a random component
 z=(p<0.5+error) # TRUE/FALSE condition
